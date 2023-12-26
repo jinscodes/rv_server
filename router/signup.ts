04314bc54843c5bc7bb4a60caa7d93f4870c22e9
{jinsung} <jayhan0215@gmail.com>
2023-12-26 19:29:20 +0900

    Post req and res

--- router/signup.ts
+++ router/signup.ts
@@ -1,11 +1,27 @@
+import { Client } from "@notionhq/client";
 import express, { Request, Response } from "express";
+import { NotionKeys } from "../src/notionKeys";
+import { SignupValues } from "./../types/signup.d";
 
 const router = express.Router();
 
+const notionKeys = new NotionKeys();
+const { notionDBUser, notionDBVWUI, notionDBVoca, notionSecret } = notionKeys;
+
+const notion = new Client({
+  auth: notionSecret,
+});
+
 router.post("/", async (req: Request, res: Response, next) => {
-  const id = req.body.id;
-  const pw = req.body.pw;
-  console.log(id, pw);
+  const body: SignupValues = req.body;
+  const lastname = body.lastname && body.lastname;
+  const firstname = body.firstname;
+  const birth = body.birth;
+  const gender = body.gender;
+  const email = body.email;
+  const id = body.id;
+  const pw = body.pw;
+  console.log(body);
 });
 
 export default router;
