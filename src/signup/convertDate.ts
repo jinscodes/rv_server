b4e5b40b157972e6030b6292f7725afc59c3341b
{jinsung} <jayhan0215@gmail.com>
2023-12-28 16:55:25 +0900

    Done Signup

--- src/signup/convertDate.ts
+++ src/signup/convertDate.ts
@@ -5,6 +5,47 @@ type Props = {
 };
 
 export const convertDate = ({ date }: Props) => {
-  console.log(date);
   const { year, month, day } = date;
+  let convertedMonth;
+
+  switch (month) {
+    case "January":
+      convertedMonth = "01";
+      break;
+    case "Febuary":
+      convertedMonth = "02";
+      break;
+    case "March":
+      convertedMonth = "03";
+      break;
+    case "April":
+      convertedMonth = "04";
+      break;
+    case "May":
+      convertedMonth = "05";
+      break;
+    case "June":
+      convertedMonth = "06";
+      break;
+    case "July":
+      convertedMonth = "07";
+      break;
+    case "August":
+      convertedMonth = "08";
+      break;
+    case "September":
+      convertedMonth = "09";
+      break;
+    case "October":
+      convertedMonth = "10";
+      break;
+    case "November":
+      convertedMonth = "11";
+      break;
+    case "December":
+      convertedMonth = "12";
+      break;
+  }
+
+  return `${year}-${convertedMonth}-${day}`;
 };
